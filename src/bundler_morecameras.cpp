e4847e3534580b371ac04cb95f55f7540aa0edf9
Daniel Cabrini Hauagge <hauagge@cs.cornell.edu>
2013-01-07 14:41:21 -0500

    created pmvs_subsample and working on bundler_morecameras

--- src/bundler_morecameras.cpp
+++ src/bundler_morecameras.cpp
@@ -62,6 +62,8 @@ main(int argc, const char* argv[])
 {
     using namespace BDATA;
     
+    int maxCamerasPerPoint = 1024;
+    
     if(argc == 1) {
       std::cout << "Usage:\n\t" << argv[0] << " <in:bundle.out> <in:list.txt> <in:list.sizes.txt> <out:visibility.dat>" << std::endl;
       return EXIT_FAILURE;
@@ -73,7 +75,6 @@ main(int argc, const char* argv[])
     const char* sizes_fname = argv[3];
     const char* visibilityListFName = argv[4];
     
-
     LOG("Loading bundle file");
     BDATA::BundlerData bundler(bundleFName);
     PRINT_VAR(bundler.getNCameras());
@@ -132,27 +133,27 @@ main(int argc, const char* argv[])
     
   vector<sizeentry> sizelist=read_sizes(sizes_fname);
   {
-    int n=sizelist.size();
+    int n = sizelist.size();
     printf("sizelist: Read %d entries.\n",sizelist.size());
     //printf("%s %d %d\n",sizelist[0].name,sizelist[0].w,sizelist[0].h);
     //printf("%s %d %d\n",sizelist[n-1].name,sizelist[n-1].w,sizelist[n-1].h);
   }
   {
     Eigen::Vector3d ctr_ww;
-    int totalnvc=0;
-    int totalnvc2=0;
-    int np=bundler.getNPoints();
-    const Camera::Vector& cam=bundler.getCameras();
-    int nc=cam.size();
-    for(int p=0;p<np;p++) {
-      PointInfo& pi=bundler.getPointInfo()[p];
-      Eigen::Vector3d p_ww=pi.position;
-      int nvc=pi.viewList.size();
+    int totalnvc = 0;
+    int totalnvc2 = 0;
+    int np = bundler.getNPoints();
+    const Camera::Vector& cam = bundler.getCameras();
+    int nc = cam.size();
+    for(int p = 0; p < np; p++) {
+      PointInfo& pi = bundler.getPointInfo()[p];
+      Eigen::Vector3d p_ww = pi.position;
+      int nvc = pi.viewList.size();
       {
         //if(p<1) printf("point %d has %d cameras\n",p,nvc);
-        totalnvc+=nvc;
-        for(int vc=0;vc<nvc;vc++) {
-          int cid=pi.viewList[vc].camera;
+        totalnvc += nvc;
+        for(int vc = 0; vc < nvc; vc++) {
+          int cid = pi.viewList[vc].camera;
           if(cam[cid].isValid()) totalnvc2++;
         }
         //if(p<1) printf("pos_ww=(%g,%g,%g) nvc=%d\n",p_ww(0),p_ww(1),p_ww(2),nvc);
@@ -160,7 +161,7 @@ main(int argc, const char* argv[])
       ctr_ww=ctr_ww+p_ww;
     }
     ctr_ww=ctr_ww/np;
-    //printf("ctr_ww=(%g,%g,%g) np=%d\n",ctr_ww(0),ctr_ww(1),ctr_ww(2),np);
+    //printf( "ctr_ww=(%g,%g,%g) np=%d\n",ctr_ww(0),ctr_ww(1),ctr_ww(2),np);
     printf("average %g cameras per point\n",(double)totalnvc/np);
     printf("average %g valid cameras per point\n",(double)totalnvc2/np);
 
@@ -185,13 +186,13 @@ main(int argc, const char* argv[])
     }
     printf("Calculating T ...\n");
     vector<Eigen::Vector3d> Tlist;
-    for(int p=0;p<np;p++) {
+    for(int p = 0; p < np; p++) {
       PointInfo& pi=bundler.getPointInfo()[p];
       Eigen::Vector3d p_ww=pi.position;
-      int nvc=pi.viewList.size();
+      int nvc = pi.viewList.size();
       vector<double> xcoord,ycoord,zcoord;
-      int m=max(min(1,nvc/300),10);
-      for(int vc=0;vc<nvc;vc++) {
+      int m = max(min(1, nvc/300), 10);
+      for(int vc = 0; vc < nvc; vc++) {
         if((vc % m)!=0) continue;
         int cid=pi.viewList[vc].camera;
         if(!cam[cid].isValid()) continue;
@@ -209,9 +210,11 @@ main(int argc, const char* argv[])
       Eigen::Vector3d bbcenter_w((xcoord[a]+xcoord[b])/2.0,(ycoord[a]+ycoord[b])/2.0,(zcoord[a]+zcoord[b])/2.0);
       Tlist.push_back(bbcenter_w.normalized());
     }
-    printf("Calculating minV ...\n");
+
+    
+    LOG("Calculating minV ...");
     vector<double> minVlist;
-    for(int p=0;p<np;p++) {
+    for(int p = 0; p < np; p++) {
       PointInfo& pi=bundler.getPointInfo()[p];
       Eigen::Vector3d p_ww=pi.position;
       int nvc=pi.viewList.size();
@@ -235,78 +238,94 @@ main(int argc, const char* argv[])
         printf("pos_wc=(%g,%g,%g) cone.direction=(%g,%g,%g) cone.halfangle=%g deg (%g deg)\n",p_wc(0),p_wc(1),p_wc(2),T_w(0),T_w(1),T_w(2),degrees(acos(minV)),degrees(acos(minvlist[0])));
       }
     }
-    printf("Calculating viewList ...\n");
-    int64_t work_units=(int64_t)np*nc; int64_t work_done=0; double work_t0=time(0);
-    double rlprint_t0=0,rlprint_dt=15;
-    unsigned char *is_original=new unsigned char[nc];
+    
+    LOG("Calculating viewList ...");
+    int64_t work_units = (int64_t) np * nc; 
+    int64_t work_done = 0; 
+    double work_t0 = time(0);
+    double rlprint_t0 = 0, rlprint_dt = 15;
+    unsigned char *is_original = new unsigned char[nc];
     //const char *oname = visibilityListFName;//"expansion.dat";
     FILE *fp=fopen(visibilityListFName, "wb");
-    if(fp==0) { perror(visibilityListFName); exit(1); }
-    for(int p=0;p<np;p++) {
-      PointInfo& pi=bundler.getPointInfo()[p];
-      Eigen::Vector3d p_ww=pi.position;
-      int nvc=pi.viewList.size();
-      Eigen::Vector3d &T_w=Tlist[p];
-      double minV=minVlist[p];
+    if(fp == 0) { 
+      perror(visibilityListFName); 
+      exit(EXIT_FAILURE); 
+    }
+
+    for(int p = 0; p < np; p++) {
+      if(p % 100 == 0) printf("%6d/%d points processed\n", p, np);
+
+      PointInfo& pi = bundler.getPointInfo()[p];
+      Eigen::Vector3d p_ww = pi.position;
+      int nvc = pi.viewList.size();
+      Eigen::Vector3d &T_w = Tlist[p];
+      double minV = minVlist[p];
       PointEntry::Vector expansion;
-      for(int cid=0;cid<nc;cid++) { is_original[cid]=0; }
-      for(int vc=0;vc<nvc;vc++) {
-        int cid=pi.viewList[vc].camera;
+
+      for(int cid = 0; cid < nc; cid++) { is_original[cid] = 0; }
+
+      for(int vc = 0; vc < nvc; vc++) {
+        int cid = pi.viewList[vc].camera;
         if(!cam[cid].isValid()) continue;
         is_original[cid]=1;
       }
+
       int ncull=0;
-      for(int cid=0;cid<nc;cid++) {
-        work_done=work_done+1;
-        if((cid % 100)==0) {
+      for(int cid = 0; cid < nc && expansion.size() < maxCamerasPerPoint; cid++) {
+        work_done = work_done + 1;
+        if((cid % 100) == 0) {
           double t1=time(0);
-          if(t1-rlprint_t0>rlprint_dt) {
-            rlprint_t0=t1;
-            printf("%" PRId64 "/%" PRId64 ", %g min remaining\n",work_done,work_units,((double)work_units/(double)work_done-1)*(t1-work_t0)/60.0);
+          if(t1 - rlprint_t0 > rlprint_dt) {
+            rlprint_t0 = t1;
+            //printf("%" PRId64 "/%" PRId64 ", %g min remaining\n", work_done, work_units, ((double)work_units/(double)work_done-1)*(t1-work_t0)/60.0);
             fflush(0);
           }
         }
+
         if(!cam[cid].isValid()) continue;
+
         Eigen::Vector3d &c_ww=cam_c_ww[cid];
         Eigen::Vector3d t_w=(c_ww-p_ww).normalized();
-        double x=T_w.dot(t_w);
-        if(T_w.dot(t_w)>=minV || is_original[cid]) {
+        double x = T_w.dot(t_w);
+        if(T_w.dot(t_w) >= minV || is_original[cid]) {
           // contraction by visibility
           // verify that the point projets into the image.
-          sizeentry &se=sizelist[cid];
-          if(se.w>0 && se.h>0) {
-            Eigen::Vector2d p_i; cam[cid].world2im(p_ww,p_i,true,se.w,se.h);
+          sizeentry &se = sizelist[cid];
+          if(se.w > 0 && se.h > 0) {
+            Eigen::Vector2d p_i; cam[cid].world2im(p_ww, p_i, true, se.w, se.h);
             //if(p<1 && cid<1000000) printf("pos_i%d = (%g,%g)pixels (%g,%g)texc\n",cid,p_i(0),p_i(1),(p_i(0)+0.5)/se.w,(p_i(1)+0.5)/se.h);
             // NOTE: these texture coordinates are upper-left
-            double u=(p_i(0)+0.5)/se.w;
-            double v=(p_i(1)+0.5)/se.h;
-            if(u>=0 && u<=1 && v>=0 && v<=1) {
-              PointEntry pe(cid,-1,Eigen::Vector2d(0,0));
+            double u = (p_i(0) + 0.5)/se.w;
+            double v = (p_i(1) + 0.5)/se.h;
+            if(u >= 0 && u <= 1 && v >= 0 && v <= 1) {
+              PointEntry pe(cid, -1, Eigen::Vector2d(0,0));
               expansion.push_back(pe);
-            }
-            else {
-              ncull=ncull+1;
+            } else {
+              ncull = ncull + 1;
             }
           }
         }
       }
-      if((p % 100)==0) printf("%d became %d (%d culled)\n",nvc,expansion.size(),ncull);
+      //if((p % 100)==0) printf("%d became %d (%d culled)\n", nvc, expansion.size(), ncull);
       if(0) pi.viewList=expansion;
       if(1) {
         // >>> struct.unpack('<1000i',f.read(4000))
-        int maxe=1024;
-        int ne=expansion.size();
-        int nemit=0;
-        int m=max(1,ne/maxe);
-        for(int e=0;e<ne && nemit<maxe;e++) {
-          if(e % m) continue;
-          int32_t cid=expansion[e].camera;
-          if(fwrite(&cid,sizeof(cid),1,fp)!=1) { perror(visibilityListFName); exit(1); }
+        int ne = expansion.size();
+        int nemit = 0;
+        //int m = max(1, ne / maxCamerasPerPoint);
+        for(int e = 0; e < ne && nemit < maxCamerasPerPoint; e++) {
+          //if(e % m) continue;
+          int32_t cid = expansion[e].camera;
+          if(fwrite(&cid, sizeof(cid), 1, fp) != 1) { 
+	    perror(visibilityListFName); exit(EXIT_FAILURE); 
+	  }
           nemit++;
         }
-        for(int e=nemit;e<maxe;e++) {
+        for(int e = nemit; e < maxCamerasPerPoint; e++) {
           int32_t pad=(-1);
-          if(fwrite(&pad,sizeof(pad),1,fp)!=1) { perror(visibilityListFName); exit(1); }
+          if(fwrite(&pad, sizeof(pad), 1, fp) != 1) { 
+	    perror(visibilityListFName); exit(EXIT_FAILURE); 
+	  }
         }
       }
       //if(p>200) break;
