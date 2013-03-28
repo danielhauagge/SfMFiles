53aab8e5a83776bc5b656bf1125caad5d7f03e8e
Daniel Cabrini Hauagge <hauagge@cs.cornell.edu>
2013-03-28 14:13:52 -0400

    Added flag to disable point visibility updates in bundler_merge

--- src/bundler_merge.cpp
+++ src/bundler_merge.cpp
@@ -47,103 +47,111 @@ basename(const std::string& fname)
 int 
 main(int argc, char const *argv[])
 {
-	using namespace BDATA;
+  using namespace BDATA;
+  
+  std::vector<std::string> args;
+  std::map<std::string, std::string> opts;
+  
+  OptionParser optParser(&args, &opts);
+  optParser.addUsage("[OPTIONS] <out:bundle.out> <out:list.txt> <in:bundle1.out> <in:list1.txt> <in:bundle2.out> <in:list2.txt> ...");
+  optParser.addDescription("This command merges bundle files and update the visibility lists for bundle files that the same number of points");
+  optParser.addFlag("dontUpdateVizList", "-d", "--no-viz-update", 
+		    "Do not update the point visibility lists (if bundle files have different number of point this must be enabled)");      
+  optParser.parse(argc, argv);
+    
+  std::string outBundleFName = args[0];
+  std::string outListFName = args[1];
+  
+  bool updateVizList = !atoi(opts["dontUpdateVizList"].c_str());
 
+  std::vector<std::string> inBundleFNames;
+  std::vector<std::string> inListFNames;
+  
+  for(int i = 2; i < args.size(); i+= 2) {
+    inBundleFNames.push_back(args[i]);
+    inListFNames.push_back(args[i + 1]);
+  }
+  
+  PRINT_MSG("Merging " << inBundleFNames.size() << " bundle files");
+  
+  std::vector<std::string> outImageList;
+  Camera::Vector outCams;
+  PointInfo::Vector outPoints;
+  
+  // We don't add a camera more than once, this keeps track of which ones
+  // we've seen.
+  std::set<std::string> imgsSeen;
+  
+  for(int bun = 0, nPoints = -1; bun < inBundleFNames.size(); bun++) {
+    PRINT_MSG("Loading " << inBundleFNames[bun]);
+    BundlerData bundle(inBundleFNames[bun].c_str());
+    bundle.loadListFile(inListFNames[bun].c_str());
+    
+    PRINT_MSG("Building index camera -> visible points");
+    bundle.buildCam2PointIndex();
+    
 
-	if(argc == 1) {
-		std::cout << "Usage:\n\t" << argv[0] << " <out:bundle.out> <out:list.txt> <in:bundle1.out> <in:list1.txt> <in:bundle2.out> <in:list2.txt> ..." << std::endl;
-		std::cout << "\nDescription:\n\tThis command merges bundle files that have the same number of points\n\tbut different number of cameras." << std::endl;
-		return EXIT_FAILURE;
-	}
-
-	const char* outBundleFName = argv[1];
-	const char* outListFName = argv[2];
-
-	std::vector<const char*> inBundleFNames;
-	std::vector<const char*> inListFNames;
-
-	for(int i = 3; i < argc; i+= 2) {
-		inBundleFNames.push_back(argv[i]);
-		inListFNames.push_back(argv[i + 1]);
-	}
-
-	PRINT_MSG("Merging " << inBundleFNames.size() << " bundle files");
-
-    std::vector<std::string> outImageList;
-    Camera::Vector outCams;
-    PointInfo::Vector outPoints;
-
-    // We don't add a camera more than once, this keeps track of which ones
-    // we've seen.
-    std::set<std::string> imgsSeen;
-
-    for(int bun = 0, nPoints = -1; bun < inBundleFNames.size(); bun++) {
-    	PRINT_MSG("Loading " << inBundleFNames[bun]);
-    	BundlerData bundle(inBundleFNames[bun]);
-    	bundle.loadListFile(inListFNames[bun]);
-
-    	PRINT_MSG("Building index camera -> visible points");
-    	bundle.buildCam2PointIndex();
-
-    	// Make sure we're not trying to merge bundle files that have different points
-    	if(bun == 0) { 
-    		nPoints = bundle.getNPoints();
-    		outPoints = bundle.getPointInfo();
-    		outCams = bundle.getCameras();
-
-    		for(int i = 0; i < bundle.getNCameras(); i++) {
-    			imgsSeen.insert(basename(bundle.getImageFileNames()[i]));
-				outImageList.push_back(bundle.getImageFileNames()[i]);
-			}
-    		continue;
-		}
-
-    	if(nPoints != bundle.getNPoints()) {
-    		PRINT_MSG("Seems like not all bundle files have the same number of points.\n>> You should only merge bundle reconstruction that have the same points but different cameras.>>I'm out!!");
-    		return EXIT_FAILURE;
-    	}
-
-		std::vector<std::string>& imageList = bundle.getImageFileNames();
-		const Camera::Vector& cams = bundle.getCameras();
-
-        int nAdded = 0;
-    	for(int imgIdx = 0; imgIdx < bundle.getNCameras(); imgIdx++) {
-    		std::string imgFName = imageList[imgIdx];
-
-    		// Make sure we're not trying to insert something we've seen already
-    		std::string imgBName = basename(imgFName);
-    		if(imgsSeen.count(imgBName) != 0) {
-                continue;
-            }
-    		imgsSeen.insert(imgBName);
-
-            nAdded++;
-
-    		// Add the new camera
-    		outCams.push_back(cams[imgIdx]);
-    		outImageList.push_back(imageList[imgIdx]);
-    		int newCamIdx = outCams.size() - 1;
-
-    		// Udate the visibility lists
-    		for(std::vector<int>::const_iterator pntIdx = cams[imgIdx].visiblePoints.begin(); pntIdx != cams[imgIdx].visiblePoints.end(); pntIdx++) {
-    			outPoints[*pntIdx].viewList.push_back(PointEntry(newCamIdx));
-    		}
-    	}
-        PRINT_MSG(std::setw(8) << nAdded << "/" << bundle.getNCameras() << " cameras were added");
-
+    if(bun == 0) { 
+      nPoints = bundle.getNPoints();
+      outPoints = bundle.getPointInfo();
+      outCams = bundle.getCameras();
+      
+      for(int i = 0; i < bundle.getNCameras(); i++) {
+	imgsSeen.insert(basename(bundle.getImageFileNames()[i]));
+	outImageList.push_back(bundle.getImageFileNames()[i]);
+      }
+      continue;
     }
 
-    PRINT_MSG("Writing bundle file to " << outBundleFName);
-    BundlerData outBundle;
-    outBundle.getCameras() = outCams;
-    outBundle.getPointInfo() = outPoints;
-    outBundle.writeFile(outBundleFName);
-
-    PRINT_MSG("Writing image list to " << outListFName);
-    std::ofstream outListF(outListFName);
-    for(int i = 0; i < outImageList.size(); i++) {
-    	outListF << outImageList[i] << std::endl;
+    // Make sure we're not trying to merge bundle files that have different points    
+    if(nPoints != bundle.getNPoints() && updateVizList) {
+      PRINT_MSG("Seems like not all bundle files have the same number of points.\n>> You should only merge bundle reconstruction that have the same points but different cameras.>>I'm out!!");
+      return EXIT_FAILURE;
     }
-
-	return EXIT_SUCCESS;
+    
+    std::vector<std::string>& imageList = bundle.getImageFileNames();
+    const Camera::Vector& cams = bundle.getCameras();
+    
+    int nAdded = 0;
+    for(int imgIdx = 0; imgIdx < bundle.getNCameras(); imgIdx++) {
+      std::string imgFName = imageList[imgIdx];
+      
+      // Make sure we're not trying to insert something we've seen already
+      std::string imgBName = basename(imgFName);
+      if(imgsSeen.count(imgBName) != 0) {
+	continue;
+      }
+      imgsSeen.insert(imgBName);
+      
+      nAdded++;
+      
+      // Add the new camera
+      outCams.push_back(cams[imgIdx]);
+      outImageList.push_back(imageList[imgIdx]);
+      int newCamIdx = outCams.size() - 1;
+      
+      // Udate the visibility lists
+      if(updateVizList) {
+	for(std::vector<int>::const_iterator pntIdx = cams[imgIdx].visiblePoints.begin(); pntIdx != cams[imgIdx].visiblePoints.end(); pntIdx++) {
+	  outPoints[*pntIdx].viewList.push_back(PointEntry(newCamIdx));
+	}
+      }
+    }
+    PRINT_MSG(std::setw(8) << nAdded << "/" << bundle.getNCameras() << " cameras were added");
+    
+  }
+  
+  PRINT_MSG("Writing bundle file to " << outBundleFName);
+  BundlerData outBundle;
+  outBundle.getCameras() = outCams;
+  outBundle.getPointInfo() = outPoints;
+  outBundle.writeFile(outBundleFName.c_str());
+  
+  PRINT_MSG("Writing image list to " << outListFName);
+  std::ofstream outListF(outListFName.c_str());
+  for(int i = 0; i < outImageList.size(); i++) {
+    outListF << outImageList[i] << std::endl;
+  }
+  
+  return EXIT_SUCCESS;
 }
