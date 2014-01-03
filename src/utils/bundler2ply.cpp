87e8bfe966cdc2e7698622a6fc5c6d34f0aa9c08
Daniel Hauagge <hauagge@cs.cornell.edu>
2014-01-02 20:28:07 -0500

    Shows camera and can also show points as seen by camera if projected into image

--- src/utils/bundler2ply.cpp
+++ src/utils/bundler2ply.cpp
@@ -50,7 +50,7 @@ recolorByNCams(Reconstruction &bundler, std::string &colorMapping)
 }
 
 void
-colorPointsSeenByCamera(Reconstruction &bundler, int camIdx, std::string &colorMapping)
+colorPointsSeenByCameraBundler(Bundler::Reconstruction &bundler, int camIdx, std::string &colorMapping)
 {
     Color red(250, 100, 100);
     Color green(100, 250, 100);
@@ -61,6 +61,25 @@ colorPointsSeenByCamera(Reconstruction &bundler, int camIdx, std::string &colorM
         for(; pe != peEnd && !found; pe++) if(pe->camera == camIdx) found = true;
 
         if(found) it->color = green;
+        else it->color = red;
+    }
+}
+
+void
+colorPointsSeenByCameraProj(Bundler::Reconstruction &bundler, int camIdx, std::string &colorMapping)
+{
+    Color red(250, 100, 100);
+    Color green(100, 250, 100);
+
+    int width, height;
+    bundler.getImageSizeForCamera(camIdx, width, height, true);
+    const Bundler::Camera &cam = bundler.getCameras()[camIdx];
+
+    for(Point::Vector::iterator it = bundler.getPoints().begin(), itEnd = bundler.getPoints().end(); it != itEnd; it++) {
+        Eigen::Vector2d im;
+        bool proj = cam.world2im(it->position, im, true, width, height);
+
+        if(proj) it->color = green;
         else it->color = red;
     }
 }
@@ -80,7 +99,9 @@ main(int argc, const char *argv[])
     optParser.addDescription("Convert Bundler reconstructions to the PLY file format.");
     // optParser.addFlag("colorByScore", "-s", "--color-score", "Change patches color to show the quality score.");
     optParser.addFlag("colorByNCams", "-c", "--color-n-cams", "Change point color to show the number of cameras a point sees.");
-    optParser.addOption("camIdx", "-s", "IDX", "--points-seen-by-cam", "Change point color to show which points are seen by a given camera.", "-1");
+    optParser.addOption("camIdx", "-b", "IDX", "--points-viz-bundler", "Change point color to show which points are seen by a given camera (as indicated in the bundle file).", "-1");
+    optParser.addOption("camIdxProj", "-p", "IDX", "--points-viz-proj", "Change point color to show which points are seen by a given camera (by projecting point into camera, requires list file to be loaded).", "-1");
+    optParser.addOption("listFName", "-l", "FNAME", "--list", "File with image filenames.");
     optParser.setNArguments(2, 2);
     optParser.parse(argc, argv);
 
@@ -90,8 +111,20 @@ main(int argc, const char *argv[])
     // bool colorByScore = opts["colorByScore"].asBool();
     bool colorByNCams = opts["colorByNCams"].asBool();
     int camIdx = opts["camIdx"].asInt();
+    int camIdxProj = opts["camIdxProj"].asInt();
+
+    bool projPoints = false;
+    if(camIdxProj) {
+        camIdx = camIdxProj;
+        projPoints = true;
+    }
+
+    std::string listFName = opts["listFName"];
 
     Reconstruction bundler(bundleFName.c_str());
+    if(listFName.size() > 0) {
+        bundler.readListFile(listFName.c_str());
+    }
 
     Ply ply;
     std::stringstream comments;
@@ -105,8 +138,17 @@ main(int argc, const char *argv[])
     } else if(camIdx >= 0) {
         comments << "Colored points that are visible to camera " << camIdx << "\n";
         std::string colorMapping;
-        colorPointsSeenByCamera(bundler, camIdx, colorMapping);
+
+        if(projPoints) colorPointsSeenByCameraProj(bundler, camIdx, colorMapping);
+        else colorPointsSeenByCameraBundler(bundler, camIdx, colorMapping);
         comments << colorMapping;
+
+        if(listFName.size() > 0) {
+            int width, height;
+            bundler.getImageSizeForCamera(camIdx, width, height, true);
+
+            ply.addCamera(bundler.getCameras()[camIdx], width, height, Ply::Color(255, 0, 0));
+        }
     }
 
     ply.addComment(comments.str());
